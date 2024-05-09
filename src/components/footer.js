994d1e214f24810f141efd65bbcd548a788d73b5
Varunjoshi22 <92745621+Varunjoshi22@users.noreply.github.com>
2024-05-09 14:15:45 +0530

    first commit

---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+// Footer.js
+
+import React from 'react';
+
+const Footer = () => {
+  return <footer className="footer">©Epics Project {new Date().getFullYear()}</footer>;
+};
+
+export default Footer;
